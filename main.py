7cc5f7e238d411ef330c55aa1460848c36a7c66b
huao cheng <huao.cheng@gmail.com>
2021-03-24 15:02:50 -0400

    Add main.py via upload
    Runs and tests the arithmetic_arranger

---
+++ main.py
@@ -0,0 +1,5 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from arithmetic_arranger import arithmetic_arranger
+
+
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"], True))
